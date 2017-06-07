3a192c2be057b6ef0a4ba77fe61604bec0089997
mahaboobfathima <mahaboobmadina@gmail.com>
2017-06-07 11:26:18 +0530

    Add files via upload

---
+++ posorneg.java
@@ -0,0 +1,19 @@
+package posorneg;
+import java.util.Scanner;
+public class posorneg {
+	public static void main(String[] args)
+	{
+		int n;
+		Scanner s = new Scanner(System.in);
+		System.out.print("enter the number you want to check:");
+		n = s.nextInt();
+	if(n > 0)
+	{
+	System.out.println("the given number "+n+" is positive");	
+	}
+	else
+	{
+		System.out.println("the given number "+n+" is negative");
+	}
+}
+}
