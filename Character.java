a6bc0d2cb804c36c9e502d19ce2cbc5928dabaae
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-03 11:53:28 +0530

    Add files via upload

--- Character.java
+++ Character.java
@@ -1,16 +1,18 @@
-package character;
+package program;
 import java.util.Scanner;
 public class Character {
 public static void main(String[] args){
 	char ch;
 	Scanner sc=new Scanner(System.in);
 	ch=sc.next().charAt(0);
-	System.out.println("enter the character");
-	if(ch>='a' && ch<='z'){
-		System.out.println(ch+"is alphabet");
+	System.out.println("enter a character:");
+	if(ch>='a'&&ch<='z')
+	{
+		System.out.println("alphabet");
 	}
-	else{
-		System.out.println(ch+"is not alphabet");
+	else
+	{
+		System.out.println("not alphabet");
 	}
 }
 }
