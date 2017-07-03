c6e3bf403d08e3356c18fe4462f7b61ade9724ef
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-03 11:21:04 +0530

    Add files via upload

---
+++ SumofN.java
@@ -0,0 +1,16 @@
+package program;
+import java.util.Scanner;
+public class SumofN {
+public static void main(String[] args){	
+	int sum=0;
+	Scanner scanner=new Scanner(System.in);
+	int number=scanner.nextInt();
+	for(int i=1;i<=number;i++)
+	{
+		sum=sum+i;
+	}
+	System.out.println("sum of numbers except given:"+sum);
+	scanner.close();
+}
+}
+
