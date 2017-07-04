36cfa99f9d98c797601ff1f124de512685098536
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-04 16:04:53 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+package program;
+
+public class Factorial {
+public static void main(String[] args){
+	int i;
+	int fact=1;
+	int number=7;
+	for(i=1;i<=number;i++){
+		fact=fact*i;
+	}
+	System.out.println("the factorial number is:"+fact);
+}
+}
