d519a5d79d204064880b5de1ff643a80c998ab06
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-04 16:00:52 +0530

    Add files via upload

---
+++ AmstrongIntervals.java
@@ -0,0 +1,25 @@
+package program;
+
+public class AmstrongIntervals {
+    public static void main(String[] args) 
+    {
+       int n, count = 0, a, b, c, sum = 0;
+       System.out.print("Armstrong numbers from 1 to 500:");
+       for(int i = 1; i <= 500; i++)
+       {
+           n = i;
+           while(n > 0)
+           {
+               b = n % 10;
+               sum = sum + (b * b * b);
+               n = n / 10;
+           }
+           if(sum == i)
+           {
+               System.out.print(i+" ");
+           }
+           sum = 0;
+       }
+   }
+}
+
