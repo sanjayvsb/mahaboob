9bf1650d91dc873f7623472d11608ad03b640dd8
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-05 09:51:14 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,11 @@
+package program;
+
+public class MultiplicationTable {
+public static void main(String[] args){
+	int num=7;
+	for(int i=1;i<=10;++i){
+
+        System.out.printf("%d * %d = %d \n", num, i, num * i);
+	}
+}
+}
