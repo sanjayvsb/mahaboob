b24775b71304183cb3e44624c24af9dd2ce6b3d3
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-05 12:37:15 +0530

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,14 @@
+package program;
+
+public class ASCII {
+	public static void main(String args[])
+    {
+        String ch;
+        int i;
+        for(i=1; i<=3; i++)
+        {
+            ch = new Character().toString();
+            System.out.print(i+ " -> " + ch + "\t");
+        }
+    }
+}
