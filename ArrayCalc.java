598f94f0f9617feb73506f7df3db2ca7465f135e
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-06 14:34:25 +0530

    Add files via upload

---
+++ ArrayCalc.java
@@ -0,0 +1,15 @@
+package Guvi;
+
+public class ArrayCalc {
+	 
+	  void sum(int a,int b){System.out.println(a+b);}  
+	  void sum(int a,int b,int c){System.out.println(a+b+c);}  
+	  
+	  public static void main(String args[]){  
+	  Calc obj=new Calc();  
+	  obj.sum(10,10,10);  
+	  obj.sum(20,20, 0);  
+	  
+	  }  
+	
+}
