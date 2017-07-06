46379d220d391f78edde2e4af54cae7ac1a50621
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-06 15:07:27 +0530

    Add files via upload

---
+++ ArrayVehicle.java
@@ -0,0 +1,15 @@
+package Guvi;
+
+public class ArrayVehicle {
+	void Vehicle()
+	{
+		System.out.println("Bike is created");
+		
+	}
+	public static void main(String[] args) 
+	{
+ArrayVehicle v=new ArrayVehicle();
+	}
+
+}
+
