b0021580ae1d033f24958ed427a1f1affd4e1729
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-06 17:19:18 +0530

    Add files via upload

---
+++ Plane.java
@@ -0,0 +1,13 @@
+package Guvi;
+
+public class Plane {
+	    public static void main(String[] args) {
+	      String str = "This is an example string.";
+	        System.out.println("Is this string starts with \"This\"? "
+	                        +str.startsWith("This"));
+	        System.out.println("Is this string starts with \"is\"? "
+	                        +str.startsWith("is"));
+	        System.out.println("Is this string starts with \"is\" after index 5? "
+	                        +str.startsWith("is", 5));
+	}
+	    }
