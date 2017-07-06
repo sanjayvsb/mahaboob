1387472d154839d5e97c4e97b6efa82d11dc9298
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-06 15:21:52 +0530

    Add files via upload

---
+++ BranchLen.java
@@ -0,0 +1,16 @@
+package Guvi;
+import java.util.Scanner;
+public class BranchLen
+{
+		public static void main(String[] args) 
+		{
+			 int a,b;
+				System.out.println("Enter the first number");
+				Scanner s=new Scanner(System.in);
+				a=s.nextInt();
+				System.out.println("Enter the second number");
+				b=s.nextInt();
+				
+		System.out.println(a!=b);  
+		    }	
+}
