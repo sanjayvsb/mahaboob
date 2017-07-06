cab170df2322b412ccc039a58503496f25669f1f
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-06 15:00:26 +0530

    Add files via upload

---
+++ ArrayCounter.java
@@ -0,0 +1,18 @@
+package Guvi;
+import java.util.Scanner;
+public class ArrayCounter 
+{
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		System.out.println("Enter the first number");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		System.out.println("Enter the second number");
+		b=s.nextInt();
+		System.out.println("The subtraction is");
+		c=a-b;
+		System.out.println(c);
+	}
+}
+
