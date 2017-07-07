3e0e378af2c0af75c505d4d61e75d8365cd35dac
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-07 09:15:58 +0530

    Add files via upload

---
+++ Graph.java
@@ -0,0 +1,20 @@
+package Guvi;
+import java.util.Scanner;
+public class Graph {
+	    public static void main(String[] args){
+	        System.out.println("enter the number sum and difference");
+	        Scanner sc=new Scanner(System.in);
+	        int a,b;
+	        int sum=sc.nextInt();
+	        int dif=sc.nextInt();
+	        int add=sum+dif;
+	        a=add/2;
+	        b=sum-a;
+	        System.out.println("a value is "+a);
+	        System.out.println("b value is  "+b);
+	       
+	       }
+	        
+	    }
+
+
