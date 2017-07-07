c8c8a0e92e02bc3bd715de888ba28da61b3e03ae
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-07 15:19:36 +0530

    Add files via upload

---
+++ BrainOrder.java
@@ -0,0 +1,28 @@
+package Guvi;
+import java.util.Scanner;
+public class BrainOrder {
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+			int k= sc.nextInt();
+			int i;
+			int sum=0;
+			int arr[]=new int[n];
+			for(i=0;i<n;i++){
+				arr[i]=sc.nextInt();
+				if(i!=k){
+					sum=sum+arr[i];
+				}
+			}
+			int b=sc.nextInt();//charged
+			sum=sum/2;
+			if(sum==b){
+				System.out.println("Bon Appetit");
+			}
+			else{
+				System.out.println(Math.abs(sum-b));
+			}
+
+		}
+
+	}
