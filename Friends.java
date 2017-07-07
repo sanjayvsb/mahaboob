57f22749c8400996fe772d2d679f3252042c8d7c
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-07 14:38:43 +0530

    Add files via upload

---
+++ Friends.java
@@ -0,0 +1,21 @@
+package Guvi;
+import java.util.Scanner;
+public class Friends {
+	  public static String chocolate(int m,int n)
+	    {
+	        if(m<=0 || n<=0)
+	        return "enter the validate rows and column";
+	        else
+	        return ((m*n)-1)+"";
+	        
+	    }
+		public static void main (String[] args) 
+		{
+	Scanner s=new Scanner(System.in);
+	System.out.println("enter the no of rows and columns in chocolate:");
+	int a=s.nextInt();
+	int b=s.nextInt();
+	System.out.print(chocolate(a,b));
+		}
+	}
+
