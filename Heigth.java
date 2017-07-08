91edf99dbcae18d312824fc930538da993847d69
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-08 13:48:27 +0530

    Add files via upload

---
+++ Heigth.java
@@ -0,0 +1,21 @@
+package Guvi;
+import java.util.*;
+import java.io.*;
+public class Heigth {
+	 public static void main(String[] args)
+	 {
+	  System.out.println("ENTER THE NUMBER OF STUDENTS: ");
+	  Scanner s=new Scanner(System.in);
+	  int n=s.nextInt();
+	  double[] Arr=new double[n];
+	  System.out.println("ENTER THE HEIGHTS: ");
+	  for(int i=0;i<n;i++)
+	   {
+	     Arr[i]=s.nextDouble();
+	    }
+	  Arrays.sort(Arr);
+	  System.out.println("<<<4th TALLEST HEIGT IS:  "+Arr[n-4]+"  >>>");
+	  for(int i=1;i<=n;i++)
+	     System.out.println("\n"+i+"th TALLEST HEIGHT IS:"+Arr[n-i]);
+	 }
+	}
