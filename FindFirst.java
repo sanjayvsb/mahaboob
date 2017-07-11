156e8d23e54e87b2b26002ca41aa21f71b4fff5f
mahaboobfathima <mahaboobmadina@gmail.com>
2017-07-11 12:57:43 +0530

    Add files via upload

---
+++ FindFirst.java
@@ -0,0 +1,14 @@
+package Guvi;
+
+public class FindFirst {
+	public static void main(String[] args) {
+		String a[]={"AF","D","EF","G"};
+		for(int c=0;c<a.length;c++) {
+		if(a[c].length()==1) {
+			System.out.println(a[c]);
+		}
+
+	}
+
+	}
+}
